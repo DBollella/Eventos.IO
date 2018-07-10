341c6f8d76ec6f3b5da33410c6cb12282f914daf
DBollella <douglas.bollella@gmail.com>
2018-07-10 00:09:49 -0300

    Implementando CQRS.

---
+++ src/Eventos.IO.Domain/Interfaces/IRepository1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Eventos.IO.Domain.Core.Models;
+
+namespace Eventos.IO.Domain.Interfaces
+{
+    public interface IRepository1<TEntity> where TEntity : Entity<TEntity>
+    {
+        void Add(TEntity obj);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(Guid id);
+        void Remove(Guid id);
+        int SaveChanges();
+        void Update(TEntity obj);
+    }
+}
