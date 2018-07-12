87536b9cbc5c4e8de514b73994cc9829eb100102
DBollella <douglas.bollella@gmail.com>
2018-07-11 23:08:59 -0300

    Continuacao do commandhandler

--- src/Eventos.IO.Domain/CommandHandlers/CommandHandler.cs
+++ src/Eventos.IO.Domain/CommandHandlers/CommandHandler.cs
@@ -2,17 +2,35 @@
 using System.Collections.Generic;
 using FluentValidation.Results;
 using System.Text;
+using Eventos.IO.Domain.Interfaces;
 
 namespace Eventos.IO.Domain.CommandHandlers
 {
     public abstract class CommandHandler
     {
+        private readonly IUnitOfWork _uow;
+
+        protected CommandHandler(IUnitOfWork uow)
+        {
+            _uow = uow;
+        }
+
         protected void NotificarValidacoesErro(ValidationResult validationResult)
         {
             foreach (var error in validationResult.Errors)
             {
                 Console.WriteLine(error.ErrorMessage);
             }
+        }
+
+        protected bool Commit()
+        {
+            // TODO: Validar se há alguma validacao de negocio com erro!
+            var commandResponse = _uow.Commit();
+            if (commandResponse.Success) return true;
+
+            Console.WriteLine("Ocorreu um erro ao salvar os dados no banco");
+            return false;
         }
     }
 }
